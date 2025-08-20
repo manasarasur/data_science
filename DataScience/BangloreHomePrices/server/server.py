64ec997f67fa39b37c71603f67e2c08f91a84968
Manasa <155431417+manasarasur@users.noreply.github.com>
2025-08-20 21:16:17 +0530

    Update server.py

--- DataScience/BangloreHomePrices/server/server.py
+++ DataScience/BangloreHomePrices/server/server.py
@@ -4,7 +4,7 @@ import util
 app = Flask(__name__)
 
 @app.route('/get_location_names', methods=['GET'])
-def get_location_names():
+def get_location_names(): 
     response = jsonify({
         'locations': util.get_location_names()
     })
